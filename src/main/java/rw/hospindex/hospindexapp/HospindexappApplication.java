0e00fd3034c176441238ad08903f05d2a91bbee6
Yusuf Mbonigaba <mbonigabay@gmail.com>
2021-09-13 21:09:46 +0200

    first commit

---
+++ src/main/java/rw/hospindex/hospindexapp/HospindexappApplication.java
@@ -0,0 +1,13 @@
+package rw.hospindex.hospindexapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospindexappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospindexappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rw/hospindex/hospindexapp/HospindexappApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.hospindex.hospindexapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospindexappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
